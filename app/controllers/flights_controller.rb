3d7fba2e217221bb2c888e0ec8890e94be62f33a
Brian-Momata <brianmomata@gmail.com>
2023-04-13 05:03:17 +0300

    Allow user to pick flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,6 +2,8 @@ class FlightsController < ApplicationController
   def index
     @departure_airports = Flight.all.map { |f| [f.departure_airport.code, f.departure_airport.id] }
     @arrival_airports = Flight.all.map { |f| [f.arrival_airport.code, f.arrival_airport.id] }
-    @flights = Flight.where("departure_airport = ?", params[:departure_code]).where("arrival_airport = ?", params[:arrival_code])
+    @flights = Flight.where("departure_airport = ?", params[:departure_code])
+                    .where("arrival_airport = ?", params[:arrival_code])
+                    .where("date(start) = ?", params[:date].to_date)
   end
 end

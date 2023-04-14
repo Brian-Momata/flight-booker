7068bf968e37930e90589aef1ad0d306a0bea9bd
Brian-Momata <brianmomata@gmail.com>
2023-04-14 04:19:04 +0300

    Generate bookings controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :flights
+
+  resources :bookings
   
   root "flights#index"
 end

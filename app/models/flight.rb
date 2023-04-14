1f9aa24dcb6a3df2b4ab0581285509f8d3f96400
Brian-Momata <brianmomata@gmail.com>
2023-04-14 03:36:24 +0300

    Generate Booking and Passenger models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport", foreign_key: "departure_airport"
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_airport"
+
+  has_many :bookings
+  has_many :passengers, through: :bookings, source: :bookings_table_foreign_key_to_passengers_table
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_072212) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_001843) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -22,6 +27,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_072212) do
     t.integer "arrival_airport"
     t.datetime "start"
     t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

43777be98f43e7180aafbd37c050d218e01d0650
Brian-Momata <brianmomata@gmail.com>
2023-04-16 05:15:47 +0300

    Remove association as bookings isn't a through table

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,5 +3,4 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_airport"
 
   has_many :bookings
-  has_many :passengers, through: :bookings, source: :bookings_table_foreign_key_to_passengers_table
 end

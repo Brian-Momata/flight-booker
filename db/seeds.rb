cefb61e2559765bd6407280513a474fce61e33e0
Brian-Momata <brianmomata@gmail.com>
2023-04-17 06:44:12 +0300

    Seed more airports and flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,18 +7,24 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# Create airports
-airport1 = Airport.create(code: "AAR")
-airport2 = Airport.create(code: "ABD")
-airport3 = Airport.create(code: "EZE")
-airport4 = Airport.create(code: "BJM")
-airport5 = Airport.create(code: "BUQ")
-airport6 = Airport.create(code: "DXB")
-airport7 = Airport.create(code: "DUS")
+# Create 50 Airport objects
+# 50.times do
+#   Airport.create(code: Faker::Travel::Airport.iata(size: 'large', region: 'united_states'))
+# end
+# Get all airports
+airports = Airport.all
 
-# Create flights with associations to airports
-Flight.create(departure_airport: airport1, arrival_airport: airport2, start:"2023-05-01 12:34:56", duration: 2)
-Flight.create(departure_airport: airport3, arrival_airport: airport1, start:"2023-05-15 09:15:30", duration: 1)
-Flight.create(departure_airport: airport5, arrival_airport: airport7, start:"2023-04-10 18:45:00", duration: 4)
-Flight.create(departure_airport: airport4, arrival_airport: airport6, start:"2023-04-20 15:30:45", duration: 5)
-Flight.create(departure_airport: airport6, arrival_airport: airport3, start:"2023-05-05 08:00:00", duration: 3)
+# Create 150 Flight objects
+150.times do
+  departure_airport = airports.sample
+  arrival_airport = airports.sample
+  start = Faker::Time.between(from:  DateTime.now - 30, to: DateTime.now + 30)
+  duration = rand(1..10)
+
+  Flight.create(
+    departure_airport: departure_airport,
+    arrival_airport: arrival_airport,
+    start: start,
+    duration: duration
+  )
+end

f66132b38e5d760504ee53e03e0aaaaf458bf6ba
Brian-Momata <brianmomata@gmail.com>
2023-05-16 05:29:31 +0300

    Set host parameter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,8 @@ Rails.application.configure do
   # Set delivery method to use 'letter_opener'
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+
+  # set the host parameter
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+
 end

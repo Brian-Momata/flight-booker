bd164f923ae259e65880e8f41ba5fafd44909730
Brian-Momata <brianmomata@gmail.com>
2023-05-16 04:20:47 +0300

    Add #confirmation_email mailer action

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,13 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def confirmation_email
+    # Save a list of passengers to a variable
+    @passengers = params[:passengers]
+
+    # Send a confimation email to all the passengers
+    @passengers.each do |passenger|
+      mail(to: passenger.email, subject: 'Booking Created')
+    end
+  end
 end
